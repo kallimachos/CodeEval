fbcea8c1904fee29433c6fb17f960e953d58e1fa
kallimachos <kallimachos@gmail.com>
2015-02-13 15:39:36 +1000

    Solve challenges

--- capitalize_words.py
+++ capitalize_words.py
@@ -7,23 +7,3 @@ for line in source:
     for word in array:
         result.append(word.replace(word[0], word[0].upper(), 1))
     print ' '.join(result)
-    
-
-   
-#Write a program which capitalizes the first letter of each word in a sentence.
-#Input sample:
-
-#Hello world
-#javaScript language
-#a letter
-#1st thing
-
-#Your program should accept as its first argument a path to a filename. Input example is the following
-#Output sample:
-#
-#Print capitalized words in the following way.
-
-#Hello World
-#JavaScript Language
-#A Letter
-#1st Thing
